cf6837e016b6f33e1c0776f51faa5af817b6f583
naaatami <144642305+naaatami@users.noreply.github.com>
2025-03-10 13:58:24 -0500

    initial setup

---
+++ firebase_config.py
@@ -0,0 +1,10 @@
+
+
+firebaseConfig = {
+  "apiKey": "AIzaSyCiRNR5N7xNoOJcDHaq-k0fEoMCfbhM5p4",
+  "authDomain": "foodfriend-a774e.firebaseapp.com",
+  "projectId": "foodfriend-a774e",
+  "storageBucket": "foodfriend-a774e.firebasestorage.app",
+  "messagingSenderId": "580311697199",
+  "appId": "1:580311697199:web:4067c1105229fc05d303da"
+};
