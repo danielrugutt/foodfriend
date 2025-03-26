6fab47b9b523d5c5e232200e59e9bfd10efc3590
Van Anderson <van.anderson@outlook.com>
2025-03-26 15:16:48 -0500

    added DietaryPreference.py, Search.py, and search_results.html

---
+++ DietaryPreference.py
@@ -0,0 +1,10 @@
+class DietaryPreference:
+    def __init__(self,exclude_cuisine,exclude_ingredients,max_sugar,intolerances,diet):
+        self.exclude_cuisine=exclude_cuisine
+        self.exclude_ingredients=exclude_ingredients
+        self.max_sugar=max_sugar
+        self.intolerances=intolerances
+        self.diet=diet
+
+    def update_preferences(self, DietaryPreference):
+        self=DietaryPreference
