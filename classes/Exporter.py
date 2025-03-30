17f576f7ed3cda4c80edec5815a93c06e444d3c7
naaatami <nataliesi@protonmail.com>
2025-03-30 11:28:54 -0500

    moved classes, added recipe

---
+++ classes/Exporter.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractmethod
+import os
+
+class Exporter(ABC):
+    @abstractmethod
+    def formatRecipe(self):
+        pass
+
+    @abstractmethod
+    def exportRecipe(self):
+        pass
+
+    @abstractmethod
+    def retrieveRecipe(self):
+        pass
+
+
+class ShareExporter(Exporter):
+    def __init__(self, recipe, outgoing_email):
+        self.api_key=os.getenv("SPOON_API_KEY")
+        self.outgoing_email = outgoing_email
+        self.recipe = recipe
+
+    def formatRecipe(self):
+        pass
+
+    def exportRecipe(self):
+        pass
+
+    def retrieveRecipe(self):
+        pass
+
+
+class DownloadExporter(Exporter):
+     def __init__(self, recipe):
+        self.api_key=os.getenv("SPOON_API_KEY")
+        self.recipe = recipe
+
+    def formatRecipe(self):
+        pass
+
+    def exportRecipe(self):
+        pass
+
+    def retrieveRecipe(self):
+        pass
--- classes/Search.py
+++ classes/Search.py
@@ -1,5 +1,5 @@
 import requests
-import DietaryPreference
+from .DietaryPreference import DietaryPreference
 import os
 
 class Search:
